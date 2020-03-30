b035a8bef1c3dec70f14640aaa7ccf22bf98d8fc
kaiqnes <kaiqnes@gmail.com>
2020-03-30 19:15:08 -0300

    Criação de estrutura básica para o backend

---
+++ src/main/java/com/omnistack/BeTheHeroApplication.java
@@ -0,0 +1,13 @@
+package com.omnistack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTheHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTheHeroApplication.class, args);
+	}
+	
+}
